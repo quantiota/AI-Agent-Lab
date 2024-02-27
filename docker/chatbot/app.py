82647d8e600caabc3c823fccd236db64d80ec411
quantiota <bouarfa.mahi@gmail.com>
2024-02-27 11:01:13 +0100

    update

--- docker/chatbot/app.py
+++ docker/chatbot/app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-	return "Hello World!"
+    return render_template('index.html', name='World')
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
