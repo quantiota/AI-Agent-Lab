d6d935a7230a96f91e33a36e75915bc11b2e64dc
quantiota <bouarfa.mahi@gmail.com>
2024-02-27 14:59:39 +0100

    update

--- docker/chatbot/app.py
+++ docker/chatbot/app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template('index.html', name='World')
+    api_key_exists = "Yes" if os.environ.get('OPENAI_API_KEY') else "No"
+    # Pass the message as a variable to the template
+    return render_template('index.html', api_key_exists=api_key_exists)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
