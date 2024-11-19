0e7f23726c8cb77afc668e764d8d9724b54fb398
Bouarfa Mahi <bouarfa.mahi.com>
2024-11-19 14:11:59 +0100

    update

--- docs/backup/scripts/recovery-microserver.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Step 1: Remove old directories
-rm -rf /home /opt
-
-# Step 2: Restore from the latest hourly backup
-cp -pr /backup/rsnapshot/hourly.1/localhost/home /
-cp -pr /backup/rsnapshot/hourly.1/localhost/opt /
